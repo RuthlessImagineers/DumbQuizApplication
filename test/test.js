41f0a33e74a1a1b963be2c54b92a6f3960c3a1a1
krishnb <kbandame@gmail.com>
2021-01-20 23:27:55 +0530

    added gitignore

--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
 var assert = require('assert');
 
-describe('Quiz', function() {
-it('should be running', function() {
-console.log("running");
+describe('Quiz', function () {
+    before('before', function () {
+        console.log("Before");
+    });
+
+    it('should be running', function () {
+        console.log("running");
+    });
+
+    after('after', function () {
+        console.log('After');
+    });
 });
-});
-  
+
